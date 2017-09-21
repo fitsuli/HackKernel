bddf39082204d0709676cb35b09eb1f692fccc43
xperiafan13-rom <androidanimerom@gmail.com>
2017-09-20 19:49:27 -0500

    ANDROID: ion: check for kref overflow Userspace can cause the kref to handles to increment arbitrarily high. Ensure it does not overflow.
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    
    Bug: 31992382
    Test: See bug for poc
    Change-Id: I6bff1df385742b1d836d43180dc87fadcea80782

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -481,6 +481,14 @@ static struct ion_handle *ion_handle_lookup(struct ion_client *client,
 	return ERR_PTR(-EINVAL);
 }
 
+/* Must hold the client lock */
+static struct ion_handle* ion_handle_get_check_overflow(struct ion_handle *handle)
+{
+	if (atomic_read(&handle->ref.refcount) + 1 == 0)
+		return ERR_PTR(-EOVERFLOW);
+	return ion_handle_get_check_overflow(handle);
+}
+
 static struct ion_handle *ion_handle_get_by_id(struct ion_client *client,
 						int id)
 {
@@ -489,10 +497,10 @@ static struct ion_handle *ion_handle_get_by_id(struct ion_client *client,
 	mutex_lock(&client->lock);
 	handle = idr_find(&client->idr, id);
 	if (handle)
-		ion_handle_get(handle);
+		return ion_handle_get_check_overflow(handle);
 	mutex_unlock(&client->lock);
 
-	return handle ? handle : ERR_PTR(-EINVAL);
+	return ERR_PTR(-EINVAL);
 }
 
 static bool ion_handle_validate(struct ion_client *client,
@@ -1472,7 +1480,7 @@ struct ion_handle *ion_import_dma_buf(struct ion_client *client, int fd)
 	/* if a handle exists for this buffer just take a reference to it */
 	handle = ion_handle_lookup(client, buffer);
 	if (!IS_ERR(handle)) {
-		ion_handle_get(handle);
+		handle = ion_handle_get_check_overflow(handle);
 		mutex_unlock(&client->lock);
 		goto end;
 	}
